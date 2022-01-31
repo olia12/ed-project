d0f70b6eff2bf35004f59b5f0a183e7328a71e0a
Popova Olga <686371olia@mail.ru>
2022-01-31 22:06:19 +0300

    Вывод переменных с использованием методов и свойств

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
-let title;
-let screens;
-let screenPrice;
-let rollback;
-let fullPrice;
-let adaptive;
-alert("Hello");
-console.log("Hello");
+let title = "ed-project";
+let screens = "Простые, Сложные, Интерактивные";
+let screenPrice = 100;
+let rollback = 6;
+let fullPrice = 20000;
+let adaptive = true;
+
+console.log(title, fullPrice, adaptive);
+console.log(screens.length);
+console.log("Стоимость верстки экранов " + screenPrice + " рублей");
+console.log("Стоимость разработки сайта " + fullPrice + " рублей");
+console.log(screens.toLowerCase().split(", "));
+console.log(fullPrice * (rollback / 100));
