6adfa92d489183af4670d394c2f1cf3f11921e17
Popova Olga <686371olia@mail.ru>
2022-02-05 02:33:15 +0300

    Создание функции типа function expression

--- script.js
+++ script.js
@@ -7,10 +7,17 @@ let adaptive = confirm("Нужен ли адаптив на сайте?");
 let service1 = prompt("Какой дополнительный тип услуги нужен?");
 let servicePrice1 = +prompt("Сколько это будет стоить?");
 let service2 = prompt("Какой дополнительный тип услуги нужен?");
-let servicePrice2 = prompt("Сколько это будет стоить?");
+let servicePrice2 = +prompt("Сколько это будет стоить?");
 let rollback = 6;
 let fullPrice = screenPrice + servicePrice1 + servicePrice2;
 let servicePercentPrice = Math.ceil(fullPrice - (fullPrice * (rollback / 100)));
+let allServicePrices;
+
+// function expression
+const getAllServicePrices = function(sp1, sp2) {
+  return sp1+sp2;
+};
+allServicePrices = getAllServicePrices(servicePrice1, servicePrice2);
 
 switch (true) {
   case fullPrice >= 30000:
@@ -27,10 +34,5 @@ switch (true) {
     break;
 }
 
-console.log(title, fullPrice, adaptive);
-console.log(screens.length);
-console.log("Стоимость верстки экранов " + screenPrice + " рублей");
-console.log("Стоимость разработки сайта " + fullPrice + " рублей");
-console.log(screens.toLowerCase().split(", "));
-console.log(fullPrice * (rollback / 100));
+
 
