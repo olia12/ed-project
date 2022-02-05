a48f7aecbf1fb26837612b2bffe00a9b41406785
Popova Olga <686371olia@mail.ru>
2022-02-05 04:21:27 +0300

    Добавление функций showTypeOf и getRollbackMessage, чистка логов

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 'use strict';
 
 let title = prompt("Как называется ваш проект?", " калькУлятор верстки");
-let screens = prompt("Какие типы экранов нужно разработать?");
+let screens = prompt("Какие типы экранов нужно разработать?", "Простые, Сложные, Интерактивные");
 let screenPrice = +prompt("Сколько будет стоить данная работа?");
 let adaptive = confirm("Нужен ли адаптив на сайте?");
 let service1 = prompt("Какой дополнительный тип услуги нужен?");
@@ -15,38 +15,53 @@ let allServicePrices;
 
 // function expression
 const getAllServicePrices = function(servicePrice1, servicePrice2) {
-  return servicePrice1 + servicePrice2;
+    return servicePrice1 + servicePrice2;
 };
-
 allServicePrices = getAllServicePrices(servicePrice1, servicePrice2);
 
 // function declaration
 function getFullPrice(screenPrice, allServicePrices) {
-  return screenPrice + allServicePrices;
+    return screenPrice + allServicePrices;
 }
-
 fullPrice = getFullPrice(screenPrice, allServicePrices);
 
 const getTitle = function(title) {
     title = title.trimStart();
     return title.charAt(0).toUpperCase() + title.substring(1).toLowerCase();
 };
-console.log(getTitle(title));
+getTitle(title);
 
-switch (true) {
-  case fullPrice >= 30000:
-    console.log("Даем скидку в 10%");
-    break;
-  case fullPrice >= 15000 && fullPrice < 30000:
-    console.log("Даем скидку в 5%");
-    break;
-  case fullPrice < 15000 && fullPrice >= 0:
-    console.log("Скидка не предусмотрена");
-    break;
-  case fullPrice < 0:
-    console.log("Что то пошло не так");
-    break;
-}
+const getServicePercentPrices = function(fullPrice, servicePercentPrice) {
+    return fullPrice - servicePercentPrice;
+};
+servicePercentPrice = getServicePercentPrices(fullPrice, servicePercentPrice);
+
+const showTypeOf = function(variable) {
+    console.log(variable, typeof variable);
+};
+
+const getRollbackMessage = function(price) {
+    switch (true) {
+        case price >= 30000:
+            return "Даем скидку в 10%";
+        case price >= 15000 && price < 30000:
+            return "Даем скидку в 5%";
+        case price < 15000 && price >= 0:
+            return "Скидка не предусмотрена";
+        case price < 0:
+            return "Что то пошло не так";
+    }
+};
+
+showTypeOf(title);
+showTypeOf(screenPrice);
+showTypeOf(adaptive);
+
+console.log(screens.toLowerCase().split(", "));
+
+console.log(getRollbackMessage(fullPrice));
+
+getServicePercentPrices(fullPrice, servicePercentPrice);
 
 
 
